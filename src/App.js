6bd7bc04ea4840ef3ff195e5d9b0b42f9b264293
Your Name <you@example.com>
2023-03-04 21:19:16 +0530

    updating footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainNavigation from "./components/MainNavigation";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainNavigation />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p>Copyright © 2023 Weightloss Tips and Hacks</p>
+    </div>
+  );
+}
+
+export default Footer;
